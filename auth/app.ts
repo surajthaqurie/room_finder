abd39d632195c1e9c7e59cee3551d1e9a3b84dd2
Suraj <surajchan68@gmail.com>
2024-04-16 19:52:58 +0545

    init: post service

--- auth/app.ts
+++ auth/app.ts
@@ -11,68 +11,68 @@ import { AuthDeleteConsumer, AuthEnableDisableConsumer, AuthUpdateConsumer, Auth
 import { dbErrorHandler } from "src/middleware";
 
 export class App {
-  public app: express.Application;
+    public app: express.Application;
 
-  constructor() {
-    this.app = express();
-    this.configureMiddlewares();
-    this.configureRoute();
-    this.dbConnector();
-    this.kafkaConfig();
-    this.errorHandlerMiddleware();
-  }
-
-  private configureMiddlewares(): void {
-    this.app.use(cors());
-
-    this.app.use(
-      morgan("dev", {
-        stream: {
-          write(message: string) {
-            const logger = Logger();
-            return logger.http(message.replace(/\n$/, ""));
-          },
-        },
-      })
-    );
-    this.app.set("rateLimit", 100);
-
-    this.app.use(helmet());
-    this.app.use("/public", express.static(path.join(__dirname, "public")));
-
-    this.app.use(express.json({ limit: "10mb" }));
-    this.app.use(express.urlencoded({ extended: false }));
-  }
-
-  private configureRoute(): void {
-    this.app.use("/api/v1", appRouter);
-  }
-
-  private dbConnector(): void {
-    new DbConnection().connect();
-  }
-
-  private async kafkaConfig() {
-    await this.kafkaTopicCreator();
-    await this.kafkaConsumer();
-  }
-
-  private async kafkaTopicCreator() {
-    new AuthUserCreateTopic(kafkaClient).createTopicIfNotExits(1);
-  }
-
-  private async kafkaConsumer() {
-    try {
-      new AuthUpdateConsumer(kafkaClient).consume();
-      new AuthEnableDisableConsumer(kafkaClient).consume();
-      new AuthDeleteConsumer(kafkaClient).consume();
-    } catch (error) {
-      console.error("consumed error:", error);
+    constructor() {
+        this.app = express();
+        this.configureMiddlewares();
+        this.configureRoute();
+        this.dbConnector();
+        this.kafkaConfig();
+        this.errorHandlerMiddleware();
     }
-  }
 
-  private async errorHandlerMiddleware() {
-    this.app.use(dbErrorHandler);
-    this.app.use(errorHandler);
-  }
+    private configureMiddlewares(): void {
+        this.app.use(cors());
+
+        this.app.use(
+            morgan("dev", {
+                stream: {
+                    write(message: string) {
+                        const logger = Logger();
+                        return logger.http(message.replace(/\n$/, ""));
+                    }
+                }
+            })
+        );
+        this.app.set("rateLimit", 100);
+
+        this.app.use(helmet());
+        this.app.use("/public", express.static(path.join(__dirname, "public")));
+
+        this.app.use(express.json({ limit: "10mb" }));
+        this.app.use(express.urlencoded({ extended: false }));
+    }
+
+    private configureRoute(): void {
+        this.app.use("/api/v1", appRouter);
+    }
+
+    private dbConnector(): void {
+        new DbConnection().connect();
+    }
+
+    private async kafkaConfig() {
+        await this.kafkaTopicCreator();
+        await this.kafkaConsumer();
+    }
+
+    private async kafkaTopicCreator() {
+        new AuthUserCreateTopic(kafkaClient).createTopicIfNotExits(1);
+    }
+
+    private async kafkaConsumer() {
+        try {
+            new AuthUpdateConsumer(kafkaClient).consume();
+            new AuthEnableDisableConsumer(kafkaClient).consume();
+            new AuthDeleteConsumer(kafkaClient).consume();
+        } catch (error) {
+            console.error("consumed error:", error);
+        }
+    }
+
+    private async errorHandlerMiddleware() {
+        this.app.use(dbErrorHandler);
+        this.app.use(errorHandler);
+    }
 }
