c627373586df1a9f3a610125a2e1e5378574915d
Suraj <surajchan68@gmail.com>
2024-04-16 20:53:01 +0545

    feat: setup postgresDB with prisma

--- posts/src/app.module.ts
+++ posts/src/app.module.ts
@@ -1,8 +1,20 @@
-import { Module } from "@nestjs/common";
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
+        ThrottlerModule.forRoot([
+            {
+                ttl: 60000, // Time to live in MS
+                limit: 10
+            }
+        ]),
+        PrismaModule
+    ],
+    controllers: [],
+    providers: []
 })
 export class AppModule {}
--- posts/src/main.ts
+++ posts/src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import * as morgan from 'morgan';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+
+    app.enableCors({ origin: true });
+    app.setGlobalPrefix('api/v1');
+    app.use(helmet());
+    app.use(morgan('dev'));
+
+    const configService = app.get(ConfigService);
+
+    const PORT = configService.get<string>('PORT') || 4003;
+    const APP_URL = configService.get<string>('APP_URL');
+
+    await app.listen(PORT, () => {
+        console.log(`Server is starting on ${APP_URL} at ${new Date()} with process id:`, process.pid);
+    });
+
+    process.on('SIGTERM', (): void => {
+        console.log('Server is closing at ', new Date());
+        app.close();
+    });
+
+    process.on('SIGINT', (): void => {
+        console.log('Server is closing at ', new Date());
+        app.close();
+    });
 }
 bootstrap();
